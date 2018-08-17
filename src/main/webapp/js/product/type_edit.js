8100db4eb01dfb2a527bdf08ee16e2497193ed54
Admin <Admin@192.168.43.244>
2018-08-17 21:10:10 +0800

    1.ztree添加在加载产品分类 信息，然后以zTree形式显示，可以调用getSelectNodes()函数，实现从选择菜单添加到文本框 2.实现保存的功能 3.实现从添加页返回到产品分类的首页 4.发现内部.js文件可以调用文件夹外部.js文件的jQuery函数

--- src/main/webapp/js/product/type_edit.js
+++ src/main/webapp/js/product/type_edit.js
@@ -1,9 +1,10 @@
+var ztree,SUCCESS=1,typeId; 
 $(document).ready(function(){
 	typeId=$('#container').data('typeId');
 	if(typeId)doGetObjectById();
 	//点击新增按钮
 	$("#editTypeForm").on("click",".lod-parent-type",showTreeLayer);
-	//选择菜单点击确定
+	//选择菜,单点击确定
 	$('#typeLayer').on('click','.btn-confirm',setTreeNodeSelected);
 	//点击取消
 	$('#typeLayer').on('click','.btn-cancle',hideTreeLayer);
@@ -12,42 +13,6 @@ $(document).ready(function(){
 	//点击确定按钮提交
 	$('#btn-save').click(doSaveOrUpdate);
 });
-//定义树的基本配置
-var setting={
-			data : {
-			    simpleData : {
-					enable : true,
-					idKey : "id",  //节点数据中保存唯一标识的属性名称
-					pIdKey : "parentId",  //节点数据中保存其父节点唯一标识的属性名称
-					rootPId : null  //根节点id
-				}
-		}
-	}
-//加载产品分类信息，然后以zTree形式显示
-function loadTypeTree(){
-	var url="productType/doFindTreeNodes.do";//???(dao-->mapper)-->service-->controller-->页面
-	$.getJSON(url,function(result){
-		if (result.state==1) {
-			//初始化zTree；
-			$.fn.zTree.init($("#typeTree"),//显示树的位置
-					setting,//树的基本位置
-					result.data);//树上要显示的数据
-		} else {
-				alert(result.message);
-		}
-	})
-}
-var ztree,SUCCESS=1,typeId; 
-var setting = {
-	data : {
-		    simpleData : {
-			enable : true,
-			idKey : "id",  //节点数据中保存唯一标识的属性名称
-			pIdKey : "parentId",  //节点数据中保存其父节点唯一标识的属性名称
-			rootPId : null  //根节点id
-		}
-	}
-}
 
 /**根据id查找ProductType对象*/
 function doGetObjectById(){
@@ -61,7 +26,6 @@ function doGetObjectById(){
 		}
 	})
 }
-
 function doFillUpdateForm(type){
 	$('#editTypeForm').data('parentId',type.parentId);
 	$('#typeNameId').val(type.name);
@@ -69,27 +33,24 @@ function doFillUpdateForm(type){
 	$('#typeSortId').val(type.sort);
 	$('#typeNoteId').html(type.note);
 }
-
 //返回
 function doBack(){
 	clearForm();
-	var url = 'product/type/listUI.do';
+	var url = 'productType/listUI.do';
 	$('#container').load(url);
 }
-
 //清空表单数据
 function clearForm(){
 	$('#editTypeForm .dynamicClear').val('');
 	$('#container').data('typeId','');
 	$('#editTypeForm').data('parentId','');
 }
-
 function doSaveOrUpdate(){
 	if($('#editTypeForm').valid()){
 		var params = doGetFormValues();
 		var typeId = $('#container').data('typeId');
 		if(typeId)params.id=typeId;
-		var url=typeId?'product/type/doUpdateObject.do':'product/type/doSaveObject.do';
+		var url=typeId?'productType/type/doUpdateObject.do':'productType/doSaveObject.do';
 		$.post(url,params,function(result){
 			if(result.state == SUCCESS){
 				alert('操作成功！');
@@ -106,7 +67,6 @@ function doGetFormValues(){
 	var parentId = $('#editTypeForm').data('parentId');
 	var sort = $('#typeSortId').val();
 	var note = $('#typeNoteId').val();
-	
 	var params = {
 			'name':name,
 			'parentId':parentId,
@@ -120,23 +80,27 @@ function showTreeLayer(){
 	$('#typeLayer').css('display','block');
 	loadTypeTree();
 }
+//加载产品分类信息，然后以zTree形式显示
+function loadTypeTree(){
+	var url="productType/doFindTreeNodes.do";//???(dao-->mapper)-->service-->controller-->页面
+	$.getJSON(url,function(result){
+		if (result.state==1) {
+			//初始化zTree；
+			ztree = $.fn.zTree.init($("#typeTree"),//显示树的位置
+					setting,//树的基本位置
+					result.data);//树上要显示的数据
+		} else {
+				alert(result.message);
+		}
+	})
+}
 //隐藏选择菜单
 function hideTreeLayer(){
 	$('#typeLayer').css('display','none');
 }
-//加载菜单树
-function loadTypeTreeData(){
-	var url = 'productType/doFindTreeNodes.do';
-	$.getJSON(url,function(result){
-		if(result.state==SUCCESS){
-			ztree = $.fn.zTree.init($("#typeTree"),setting,result.data);
-		}else{
-			alert(result.message);
-		}
-	})
-}
 //获取选择菜单选中项
 function setTreeNodeSelected(){
+	//debugger
 	 var node = ztree.getSelectedNodes();
 	 var parentId = node[0].id;
 	 $('#typeLayer').css('display','none');
